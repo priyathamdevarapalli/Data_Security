70bad7c81bc6b20d524df0e035d7158ee9f487e9
priyathamdevarapalli <108664363+priyathamdevarapalli@users.noreply.github.com>
2023-01-13 15:34:21 +0530

    Update Database_Security.tf

--- Database_Security.tf
+++ Database_Security.tf
@@ -11,7 +11,7 @@ resource "aws_ebs_snapshot" "example_snapshot" {
   volume_id = aws_ebs_volume.example.id
   encrypted = false
   tags = {
-    Name = "HelloWorld_snap"
+    Name = "HelloWorld_snap-"
   }
 }
 
