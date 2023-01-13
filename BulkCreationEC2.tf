af5635e970dbf6d657dc8ec31acdf07538be83cc
priyathamdevarapalli <108664363+priyathamdevarapalli@users.noreply.github.com>
2023-01-13 18:43:43 +0530

    Update BulkCreationEC2.tf

--- BulkCreationEC2.tf
+++ BulkCreationEC2.tf
@@ -1,1 +1,18 @@
-
+resource "aws_instance" "my-machine" {
+  # Creation of 11 instances
+  count = 11
+  ebs_optimized = true
+  monitoring = true
+  metadata_options {
+           http_endpoint = "enabled"
+           http_tokens   = "required"
+  }
+  # All four instances will have the same ami and instance_type
+  ami = lookup(var.ec2_ami,var.region) 
+  instance_type = var.instance_type # 
+  tags = {
+    # The count.index allows you to launch a resource 
+    # starting with the distinct index number 0 and corresponding to this instance.
+    Name = "my-machine-${count.index}"
+  }
+}
