6eee19adfb9228b7fccd090f3237c8e02c6b240b
Deloitte <ppriyathamreddy@deloitte.com>
2023-01-13 18:22:51 +0530

    first commit

---
+++ BulkCreationEC2.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "my-machine" {
+  # Creates twelve identical aws ec2 instances
+  count = 123
+  
+  # All four instances will have the same ami and instance_type
+  ami = lookup(var.ec2_ami,var.region) 
+  instance_type = var.instance_type # 
+  tags = {
+    # The count.index allows you to launch a resource 
+    # starting with the distinct index number 0 and corresponding to this instance.
+    Name = "my-machine-${count.index}"
+  }
+}
