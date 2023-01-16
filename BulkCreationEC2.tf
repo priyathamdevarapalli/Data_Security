484fcf32bef1cdeed909ff11e2597a44599a0079
Deloitte <ppriyathamreddy@deloitte.com>
2023-01-16 13:19:48 +0530

    Demo

--- BulkCreationEC2.tf
+++ BulkCreationEC2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "my-machine" {
   # Creating More than 10 instances will be considered as a violation
-  count = 32
+  count = 11
   ebs_optimized = true
   monitoring = true
   metadata_options {
